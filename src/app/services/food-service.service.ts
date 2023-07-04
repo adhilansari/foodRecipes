03e3b7d8b60eb3dc9be8727f0663b5e2753afd0f
Adhil Ansari <frkadhilansaripv@gmail.com>
2023-07-04 18:26:43 +0530

    😶‍🌫️

--- src/app/services/food-service.service.ts
+++ src/app/services/food-service.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { IFoodHits, IFoodObject } from '../interfaces/food.interface';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ import { IFoodHits, IFoodObject } from '../interfaces/food.interface';
 export class FoodServiceService {
 
   constructor(private http:HttpClient) { }
-  APP_KEY:string='3af2e53ffca4b87028ca91f32d90fd3b'
-  APP_ID:string='dcfc279d'
+  APP_KEY=environment.APP_KEY;
+  APP_ID=environment.APP_ID
 
   getSearchResult(searchKey:string):Observable<IFoodHits[]>{
     return this.http.get<IFoodObject>(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchKey}s&app_id=${this.APP_ID}&app_key=${this.APP_KEY}`).pipe(map(val=>val.hits))
@@ -18,5 +19,4 @@ export class FoodServiceService {
   getFood(link:string){
     return this.http.get<IFoodHits>(link)
   }
-
 }
